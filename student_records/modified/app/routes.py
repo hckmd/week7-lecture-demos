0016990aebe3ac43529c44b63993338dd75ef3f6
hckmd <daniel.hickmott@gmail.com>
2021-09-22 08:10:22 +1000

    Changes student records example to use Flask-SQLA and save to db

--- student_records/modified/app/routes.py
+++ student_records/modified/app/routes.py
@@ -2,7 +2,8 @@ import csv
 
 from flask import render_template, request
 
-from app import app
+from app import app, db
+from .models import Student
 
 def load_students():
     # A utility function to load the students' grades from the csv file
@@ -24,25 +25,18 @@ def index():
 def add_student():
     # Check if the form has been submitted (is a POST request)
     if request.method == 'POST':
-        # Get data from the form and put in dictionary
-        student = {}
-        student['name'] = request.form.get('student_name')
-        student['english_mark'] = request.form.get('english_mark')
-        student['science_mark'] = request.form.get('science_mark')
-        student['mathematics_mark'] = request.form.get('mathematics_mark')
-        student['does_homework'] = request.form.get('does_homework') == 'on'
-        student['stays_on_task'] = request.form.get('stays_on_task') == 'on'
+        # Get data from the form and put in a Student object
+        student = Student (
+            name = request.form.get('student_name'),
+            english_mark = request.form.get('english_mark'),
+            science_mark = request.form.get('science_mark'),
+            mathematics_mark = request.form.get('mathematics_mark'),
+            does_homework = request.form.get('does_homework') == 'on',
+            stays_on_task = request.form.get('stays_on_task') == 'on'
+        )
 
-        # Load the students from the CSV file and add the new student
-        students = load_students()
-        students.append(student)
-
-        # Open up the csv file and overwrite the contents
-        with open('students.csv', 'w', newline='') as file:
-            fieldnames = ['name', 'english_mark', 'science_mark', 'mathematics_mark', 'does_homework', 'stays_on_task']
-            writer = csv.DictWriter(file, fieldnames = fieldnames)
-            writer.writeheader()
-            writer.writerows(students)
+        db.session.add(student)
+        db.session.commit()
         
         # Returns the view with a message that the student has been added
         return render_template('add_success.html', student = student)
