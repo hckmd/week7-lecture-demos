a509728d6dbe38e36804e92991c7aab13cf1485e
hckmd <daniel.hickmott@gmail.com>
2021-09-22 15:55:13 +1000

    Changes forms.py to include an example form for registering for a mailing list

--- student_records/with_wtf/app/forms.py
+++ student_records/with_wtf/app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, BooleanField, SubmitField
-from wtforms.validators import InputRequired
+from wtforms.validators import Email, InputRequired, Length
 
 class AddStudentForm(FlaskForm):
     name = StringField('Student name', validators=[InputRequired()])
@@ -10,3 +10,12 @@ class AddStudentForm(FlaskForm):
     does_homework = BooleanField('Does homework?')
     stays_on_task = BooleanField('Stays on task?')
     submit = SubmitField('Add student')
+
+# Example form for use in slides (not actually implemented in this project)
+class RegisterMailingListForm(FlaskForm):
+    first_name = StringField('First name', validators=[InputRequired(), Length(min=1, max=40)])
+    last_name = StringField('Last name', validators=[InputRequired(), Length(min=1, max=40)])
+    email_address = StringField('Email address', validators=[InputRequired(), Length(min=1, max=40)])
+    interested_in_sports = BooleanField('Interested in sports?')
+    interested_in_technology = BooleanField('Interested in technology?')
+    submit = SubmitField('Register')
